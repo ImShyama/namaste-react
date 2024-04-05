7e62511f374c0fffcb2f3e691f74d2b02ffee8fe
Shyama pandit <P.shyama96@gmail.com>
2024-04-05 23:55:23 +0545

    Episode-02

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
-
-
-/**  <div id="parent">
-*       <div id="child">
-*            <h1></h1>
-*            <h2></h2>
-*       </div>
-*       <div id="child2">
-*            <h1></h1>
-*            <h2></h2>
-*       </div>
-*    </div> 
-*
-*/
+import React from "react";
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement("div", { id: "parent" },
     [
         React.createElement("div", { id: "child1" },
             [
-                React.createElement("h1", { id: "heading" }, "Hello World from React!"),
+                React.createElement("h1", { id: "heading" }, "This is a namaste React!"),
                 React.createElement("h2", { id: "heading" }, "Hello World from React demo!")
             ]
         ),
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-Nameste React👨‍💻
+# Nameste React 👨‍💻
+
+Episode-02
+- npm init
+- 
+
+
+# Parcel - parceljs.org
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm = written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code 
+- Different dev and prod bundles
+
+# Browser List - browserslist.dev
